95c16bd971d1f9c860c081913e35f4161b33348e
sKolev03 <stiliyankolev03@gmail.com>
2025-11-28 20:25:59 +0200

    Refactor search

--- src/services/movieSrevice.js
+++ src/services/movieSrevice.js
@@ -2,18 +2,33 @@ import { Types } from 'mongoose';
 import Movie from '../models/Movie.js';
 
 export default {
-    async getAll(filter) {
-        const result = await Movie.find(filter);      
+    getAll(filter) {
+        let query = Movie.find();      
         // const result = await Movie.find(filter).lean();
         // const resultObj = result.map(m => m.toObject());
 
-        // result.forEach(movie => console.log(movie._id instanceof Types.ObjectId));
+        if (filter.title) {
+            // TODO Search by title, partial match, case insensitive
+            query = query.find({ title: { $regex: filter.title, $options: 'i' } });
+        }
 
-        return result;
+        if (filter.genre) {
+            // TODO Search by genre, exact match, casse insensitive
+            query = query.find({ genre: { $regex: new RegExp(`^${filter.genre}$`), $options: 'i' } });
+        }
+
+        if (filter.year) {
+            // TODO Search by year, exact match
+                // result = result.find({ year: filter.year });
+            query = query.where('year').equals(filter.year);
+        }
+
+        return query;
     },
 
     getOne(movieId) {
-        return Movie.findOne({_id: movieId});
+        // return Movie.findOne({_id: movieId});
+        return Movie.findById(movieId);
     },
 
     create(movieData) {
